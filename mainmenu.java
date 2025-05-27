5f5da1243206bf256a85cc13c6cae3446c0f3dc3
matiasdiale <matiasdiale@gmail.com>
2025-05-27 19:13:17 -0300

    Create mainmenu.java
    Creando el menú y posibles errores

---
+++ mainmenu.java
@@ -0,0 +1,41 @@
+com.techlab.menu;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public static void main(String[] args) {
+
+   Scanner entrada = new Scanner(System.in);
+   
+   int opcionUsuario = 0;
+
+//int AgregProdu = 0, listaProdu = 0, buscaActuali = 0, eliminProducto = 0; creaPedido = 0, listaPedido = 0, salir = 0,
+
+system.out.println("""
+                    1) Agregar producto
+                    2) Listar productos
+                    3) Buscar/Actualizar producto
+                    4) Eliminar producto
+                    5) Crear un pedido
+                    6) Listar pedidos
+                    7) Salir
+Elija una opción:
+                   """);
+
+
+ // "String" nombre = entrada.nextLine();
+
+ // "int edad" = entrada.nextInt();
+
+     int opcionUsuario = entrada.nextInt();
+
+ //if (){}else{}
+
+   if (opcionUsuario < 1){
+        System.out.println("Ingrese una opción válida");
+   }else{ if (opcionUsuario > 7){
+          }else{ System.out.println("Ingrese una opción válida del menú");
+          }
+       }
+   } 
