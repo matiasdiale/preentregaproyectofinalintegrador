7599f2f39a10d26a7a79342b31973d6d60985c80
matiasdiale <matiasdiale@gmail.com>
2025-05-27 19:33:25 -0300

    Update mainmenu.java

--- mainmenu.java
+++ mainmenu.java
@@ -1,43 +1,43 @@
-com.techlab.menu;
+package preentregaproyectofinalintegrador;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainMenu {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-   Scanner entrada = new Scanner(System.in);
-   
-   int opcionUsuario = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        int opcionUsuario = 0;
 
 //int AgregProdu = 0, listaProdu = 0, buscaActuali = 0, eliminProducto = 0; creaPedido = 0, listaPedido = 0, salir = 0,
 
-system.out.println("""
-                    1) Agregar producto
-                    2) Listar productos
-                    3) Buscar/Actualizar producto
-                    4) Eliminar producto
-                    5) Crear un pedido
-                    6) Listar pedidos
-                    7) Salir
-Elija una opción:
-                   """);
+        system.out.println("""
+                 1) Agregar producto
+                 2) Listar productos
+                 3) Buscar/Actualizar producto
+                 4) Eliminar producto
+                 5) Crear un pedido
+                 6) Listar pedidos
+                 7) Salir
+                 Elija una opción:
+                """);
 
+        // "String" nombre = entrada.nextLine();
 
- // "String" nombre = entrada.nextLine();
+        // "int edad" = entrada.nextInt();
 
- // "int edad" = entrada.nextInt();
+        opcionUsuario = entrada.nextInt();
 
-     int opcionUsuario = entrada.nextInt();
+        //if (){}else{}
 
- //if (){}else{}
-
-   if (opcionUsuario < 1){
-        System.out.println("Ingrese una opción válida");
-   }else{ if (opcionUsuario > 7){
-          }else{ System.out.println("Ingrese una opción válida del menú");
-          }
-       }
-   } 
+        if (opcionUsuario < 1) {
+            System.out.println("Ingrese una opción válida");
+        } else {
+            if (opcionUsuario > 7) {
+            } else {
+                System.out.println("Ingrese una opción válida del menú");
+            }
+        }
+    }
 }
